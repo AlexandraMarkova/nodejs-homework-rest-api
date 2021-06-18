a79cba09e4dec686671976bcdb09c4ef19001afa
AlexandraMarkova <a.markova@ukr.net>
2021-06-18 15:03:16 +0300

    меняет структуру app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./src/routes/api/contacts')
 
 const app = express()
 
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-const { HttpCode } = require('./helpers/constants')
+const { HttpCode } = require('./src/helpers/constants')
 
 app.use((req, res, next) => {
   res.status(HttpCode.NOT_FOUND).json({
