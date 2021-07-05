255fffeb0f0b74a90dcc884378560127a42afab9
AlexandraMarkova <a.markova@ukr.net>
2021-07-05 16:23:34 +0300

    переносит аватарку в avatars и дает уникальное имя

--- src/controllers/controllersFiles.js
+++ src/controllers/controllersFiles.js
@@ -1,9 +1,18 @@
 const { HttpCode } = require('../helpers/constants')
+const fs = require('fs').promises
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
+
+const IMG_DIR = path.resolve('./public/avatars')
 
 const uploadController = async (req, res, next) => {
   console.log(req.file)
+  const [, extension] = req.file.originalname.split('.')
+
+  const fileName = path.join(IMG_DIR, `${uuidv4()}.${extension}`)
   try {
     if (req.file) {
+      await fs.rename(req.file.path, fileName)
       return res.status(HttpCode.OK).json({
         status: 'success',
       })
--- src/routes/api/files.js
+++ src/routes/api/files.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 const path = require('path')
 // const { v4: uuidv4 } = require('uuid')
 
-const UPLOAD_DIR = path.resolve('./public/avatars')
+const UPLOAD_DIR = path.resolve('./tmp')
 
 const router = express.Router()
 
